f9c3243071b565aab91dd4dcf6ce135b8d103b25
Michael <michael.sauerbraten@gmail.com>
2009-12-02 16:33:35 -0500

    Organized tokens, added CTF messages.

--- main.rb
+++ main.rb
@@ -79,9 +79,18 @@ class DemoParser
             case token
                 when 0x02 # SV_WELCOME
                     buffer.read_int
+                when 0x03 # SV_INITCLIENT
+                    client_num = buffer.read_int
+                    player = @players[client_num]
+                    player[:name], player[:team] = buffer.read_string(2)
+                    player[:model] = buffer.read_int
+                when 0x11 # SV_SPAWN
+                    buffer.read_int(12)
                 when 0x15 # SV_MAPCHANGE
                     map_name = buffer.read_string
                     game_mode, not_got_items = buffer.read_int(2)
+                when 0x1c # SV_CLIENTPING
+                    @players[client][:ping] = buffer.read_int
                 when 0x1d # SV_TIMEUP
                     buffer.read_int
                 when 0x22 # SV_RESUME
@@ -89,27 +98,31 @@ class DemoParser
                         if client_num < 0 then break end
                         @players[client_num][:info] = buffer.read_int(15)
                     end
-                when 0x03 # SV_INITCLIENT
-                    client_num = buffer.read_int
-                    player = @players[client_num]
-                    player[:name], player[:team] = buffer.read_string(2)
-                    player[:model] = buffer.read_int
-                when 0x4f # SV_INITFLAGS
-                    buffer.read_int(3)
                 when 0x51 # SV_CLIENT
                     client_num = buffer.read_int
                     parse_messages(buffer.sub_buffer(buffer.read_uint), client_num)
-                when 0x1c # SV_CLIENTPING
-                    @players[client][:ping] = buffer.read_int
-                when 0x11 # SV_SPAWN
-                    buffer.read_int(12)
                 when 0x56 # SV_PAUSEGAME
                     buffer.read_int
-                when 0x20 # SV_SERVMSG
-                    p remove_colors(buffer.read_string)
+
+                # Change player state
                 when 0x07 # SV_CDIS - client disconnected
                     client_num = buffer.read_int
                     puts 'Disconnect: %s (%d)' % [@players[client_num][:name],client_num]
+                when 0x36 # SV_CURRENTMASTER - player becomes master
+                    @players[buffer.read_int][:priv] = buffer.read_int
+                    # Clear all other privileges?
+                when 0x37 # SV_SPECTATOR - player becomes spectator, or not spectator?
+                    @players[buffer.read_int][:spectator] = buffer.read_int == 1
+                when 0x39 # SV_SETTEAM
+                    @players[buffer.read_int][:team] = buffer.read_string
+                when 0x5f # SV_SWITCHNAME
+                    name = buffer.read_string
+                    puts 'Renamed %s -> %s' % [@players[client][:name], name]
+                    @players[client][:name] = name
+                when 0x60 # SV_SWITCHMODEL
+                    buffer.read_int
+
+                # In game tokens
                 when 0x0e # SV_SHOTFX - weapon fired
                     buffer.read_int(7)
                 when 0x0c # SV_DAMAGE - player hit?
@@ -118,23 +131,29 @@ class DemoParser
                     buffer.read_int(6)
                 when 0x0b # SV_DIED - player died?
                     buffer.read_int(3)
-                when 0x37 # SV_SPECTATOR - player becomes spectator, or not spectator?
-                    @players[buffer.read_int][:spectator] = buffer.read_int == 1
-                when 0x36 # SV_CURRENTMASTER - player becomes master
-                    @players[buffer.read_int][:priv] = buffer.read_int
-                    # Clear all other privileges?
                 when 0x06 # SV_SOUND
                     buffer.read_int
-                when 0x60 # SV_SWITCHMODEL
+                when 0x13 # SV_GUNSELECT
                     buffer.read_int
-                when 0x39 # SV_SETTEAM
-                    @players[buffer.read_int][:team] = buffer.read_string
-                when 0x5f # SV_SWITCHNAME
-                    name = buffer.read_string
-                    puts 'Renamed %s -> %s' % [@players[client][:name], name]
-                    @players[client][:name] = name
+                
+                # Player messages
                 when 0x05 # SV_TEXT
                     puts '%s: %s' % [@players[client][:name], buffer.read_string]
+                when 0x20 # SV_SERVMSG
+                    p remove_colors(buffer.read_string)
+                
+                # CTF mode tokens
+                when 0x48 # SV_TAKEFLAG
+                    buffer.read_int(2)
+                when 0x49 # SV_RETURNFLAG
+                    buffer.read_int(2)
+                when 0x4d # SV_DROPFLAG
+                    buffer.read_int(5)
+                when 0x4e # SV_SCOREFLAG
+                    buffer.read_int(5)
+                when 0x4f # SV_INITFLAGS
+                    buffer.read_int(3)
+
                 else
                     puts 'Failed (at %d): %02x' % [buffer.position-1, token]
                     buffer.read_int(3).each { |i| if not i.nil? then puts '%02x' % i end }
@@ -150,5 +169,5 @@ class DemoParser
     end
 end
 
-DemoParser.new('2009_11_29_19_30_55-instagib_team-ot-psl_match_-_}tc{hero_vs.__rb_honzik1').parse
-#DemoParser.new('2009_11_30_00_50_35-insta_ctf-forge').parse
+#DemoParser.new('2009_11_29_19_30_55-instagib_team-ot-psl_match_-_}tc{hero_vs.__rb_honzik1').parse
+DemoParser.new('2009_11_30_00_50_35-insta_ctf-forge').parse
